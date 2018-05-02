aa03e3a3a69b96915ecd1707eb3af5a1eb29a1f1
simohamed <simohamed@gmail.com>
2018-05-02 15:30:37 -0400

    test

---
+++ RestDataJpaSpringBoot/src/main/java/com/simo/RestDataJpaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.simo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDataJpaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDataJpaSpringBootApplication.class, args);
+	}
+}
