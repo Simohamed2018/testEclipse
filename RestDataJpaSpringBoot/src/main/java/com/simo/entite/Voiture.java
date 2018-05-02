0080671212104ccce4151cf5753f37fd9f4ec11c
simohamed <simohamed@gmail.com>
2018-05-02 15:45:07 -0400

    adda Voiture Classe

---
+++ RestDataJpaSpringBoot/src/main/java/com/simo/entite/Voiture.java
@@ -0,0 +1,25 @@
+package com.simo.entite;
+
+public class Voiture {
+	private String porte;
+	private String fenetere;
+
+	public String getFenetere() {
+		return fenetere;
+	}
+
+	public void setFenetere(String fenetere) {
+		this.fenetere = fenetere;
+	}
+
+	public String getPorte() {
+		return porte;
+	}
+
+	public void setPorte(String porte) {
+		this.porte = porte;
+	}
+	
+	
+
+}
