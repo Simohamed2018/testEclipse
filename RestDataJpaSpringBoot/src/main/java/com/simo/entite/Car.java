6ea27a5fdef6dc10c8b4e1522fce23bedbd2c612
simohamed <simohamed@gmail.com>
2018-05-02 15:39:38 -0400

    create classe Car

---
+++ RestDataJpaSpringBoot/src/main/java/com/simo/entite/Car.java
@@ -0,0 +1,16 @@
+package com.simo.entite;
+
+public class Car {
+	private String porte;
+
+	public String getPorte() {
+		return porte;
+	}
+
+	public void setPorte(String porte) {
+		this.porte = porte;
+	}
+	
+	
+
+}
