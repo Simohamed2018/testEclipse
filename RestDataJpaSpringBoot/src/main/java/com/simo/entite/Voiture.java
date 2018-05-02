e51c5ffcc4331a150abe8ba99b4560352a470908
simohamed <simohamed@gmail.com>
2018-05-02 16:28:21 -0400

    adda hjas code and equal

--- RestDataJpaSpringBoot/src/main/java/com/simo/entite/Voiture.java
+++ RestDataJpaSpringBoot/src/main/java/com/simo/entite/Voiture.java
@@ -30,6 +30,38 @@ public class Voiture {
 	public String toString() {
 		return "Voiture [porte=" + porte + ", fenetere=" + fenetere + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fenetere == null) ? 0 : fenetere.hashCode());
+		result = prime * result + ((porte == null) ? 0 : porte.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voiture other = (Voiture) obj;
+		if (fenetere == null) {
+			if (other.fenetere != null)
+				return false;
+		} else if (!fenetere.equals(other.fenetere))
+			return false;
+		if (porte == null) {
+			if (other.porte != null)
+				return false;
+		} else if (!porte.equals(other.porte))
+			return false;
+		return true;
+	}
+	
 	
 	
 
